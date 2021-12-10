b146d7c47c69a8e1d3f6e7563a88753501b6c5ad
muftic <marijan.muftic@gmail.com>
2021-12-10 14:00:12 +0100

    fixed backslash

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,2 @@
-export const apiUrl =
-  process.env.API_URL || "https://piction-ai.herokuapp.com/";
+export const apiUrl = process.env.API_URL || "https://piction-ai.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
